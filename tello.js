fd44913a3cf0435f56369a0e9895a54129bc2980
Chris Stewart <mcstewart76@gmail.com>
2022-05-03 11:57:40 -0400

    Updated code to add autonomous section to run on certain commands

--- tello.js
+++ tello.js
@@ -1,5 +1,16 @@
 const dgram = require('dgram');
 const readline = require('readline');
+// const keymage = require('keymage')
+
+
+
+// keymage('w', function() {console.log("forward 10"); });
+// keymage('q', function() {console.log("ccw 15"); });
+// keymage('e', function() {console.log("cw 15"); });
+// keymage('s', function() {console.log("back 10"); });
+// keymage('w', function() {console.log("forward 10"); });
+// keymage('w', function() {console.log("forward 10"); });
+
 
 const PORT = 8889;
 const HOST = '192.168.10.1';
@@ -10,7 +21,11 @@ client.bind(PORT);
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+        
+        }
+      );
+
+
 
 function sendMessage(command) {
     client.send(command, 0, command.length, PORT, HOST, (err,bytes) => {
@@ -23,16 +38,29 @@ client.on('message', (message, remote) => {
     console.log(`${remote.address}:${remote.port} - ${message}`);
 })
 
-function userInput(){
+ function userInput(){
     rl.question('>>> ', (command) => {
         if (command === 'exit'){
             console.log("\n \n Program exiting, Goodbye! \n \n")
             process.exit();
+        }if(command === 'chris'){
+            sendMessage('takeoff');
+             sleep(1000);
+             sendMessage('up 20');
+             sleep(1000);
+             sendMessage('forward 20');
+             sleep(1000);
+            sendMessage('land');
+        userInput();
         }else{
             sendMessage(command)
             userInput();
         }
     });
 };
-
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
 userInput();
